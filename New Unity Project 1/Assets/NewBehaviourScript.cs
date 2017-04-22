2e24f418bb6b81217ef9822ce9209763292ee7cd
MunoDevelop <huozuyinshua@gmail.com>
2017-04-23 08:29:44 +0900

    new

---
+++ New Unity Project 1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public void onclick()
+    {
+        Debug.Log("oh");
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New Unity Project 1/Assets/newscc.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class newscc : MonoBehaviour {
+    public GameObject mask;
+    public GameObject uicamera;
+    public GameObject UIeventsystem;
+
+     bool uion = false;
+
+   
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyUp("1")){
+            if (uion==false)
+            {
+                mask.GetComponent<SpriteRenderer>().enabled = true;
+                uicamera.GetComponent<Camera>().enabled = true;
+                UIeventsystem.GetComponent<EventSystem>().enabled = true;
+                Time.timeScale = 0;
+                uion = true;
+            }else 
+            {
+                mask.GetComponent<SpriteRenderer>().enabled = false;
+                uicamera.GetComponent<Camera>().enabled = false;
+                UIeventsystem.GetComponent<EventSystem>().enabled = false;
+                Time.timeScale = 1;
+                uion = false;
+            }
+        }
+
+		
+	}
+}
